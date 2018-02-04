d095d8baaf792be45b3fc935b3dbc89803196533
Code Not Acceptable <36109227+code406@users.noreply.github.com>
2018-02-04 17:58:35 +0100

    Minor style issues fixed

--- game_loop.c
+++ game_loop.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 	}
 
 
-  /*
+        /*
 	Intenta crear el juego a partir de game_data_file, que es el argv[1]
 	Si da error, muestra mensaje y termina (return)
 	*/
