26462f9fb100763d175a63f609cc764e873e6bfa
Code Not Acceptable <36109227+code406@users.noreply.github.com>
2018-02-10 18:50:51 +0100

    Added 2 new toggles for command
    Add to master. Comments from "david" havent been added

--- command.c
+++ command.c
@@ -13,14 +13,14 @@
 #include "command.h"
 
 #define CMD_LENGHT 30
-#define N_CMD 5
+#define N_CMD 7
 
 /*Tabla de cinco elementos del tipo puntero a char*/
-char *cmd_to_str[N_CMD] = {"No command", "Unknown", "Exit", "Following", "Previous"};
+char *cmd_to_str[N_CMD] = {"No command", "Unknown", "Exit", "Following", "Previous","Get","Drop"};
 
 /*Tabla de cinco elementos del tipo puntero a char*/
 /*Abreviación de la tabla anterior*/
-char *short_cmd_to_str[N_CMD] ={"","","e","f","p"};
+char *short_cmd_to_str[N_CMD] ={"","","e","f","p","g","d"};
 
 /*Definición de la Función, no tiene argumentos*/
 T_Command get_user_input(){
@@ -28,7 +28,7 @@ T_Command get_user_input(){
   T_Command cmd = NO_CMD;
   /*Variable input tipo cadena de caracteres, leerá el comando más adelante*/
   char input[CMD_LENGHT] = "";
-  /*Variable i tipo entero inicializada a 2*/
+  /*Variable i tipo entero inicializada a 0*/
   int i=UNKNOWN - NO_CMD + 1;
 
   /*Si escanea el comando bien*/
